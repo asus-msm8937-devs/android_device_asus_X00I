7bd36127304a45c83d6f8ffc29a8793aeb862ee6
Anton Hansson <hansson@google.com>
2019-01-18 14:10:45 +0000

    X00I: Use TARGET_SCREEN_DENSITY
    This variable is used to auto-populate the ro.sf.lcd_density as
    well as selecting density bucket for resources in recovery.
    
    Bug: 118676832
    Test: adb shell getprop ro.sf.lcd_density
    Test: boot into recovery
    Change-Id: Ib383d18b61e7a2058a69ae26136aa053fdba4df2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -32,9 +32,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
     $(LOCAL_PATH)/overlay/packages/apps/Snap
 
-# Properties
--include $(LOCAL_PATH)/vendor_prop.mk
-
 # Camera
 PRODUCT_PACKAGES += \
     libgui_vendor
