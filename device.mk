681ee5d9a276d3a9da42861c8e90203e0163bc37
Quallenauge <Hamsi2k@freenet.de>
2019-11-12 22:49:17 +0100

    X00I: Exclude Snap overlays from RRO
    * Snap overlays doesn't work with RRO
    
    Change-Id: I781ce4c723cd69adb2cf568c3a31b385468444e8

--- device.mk
+++ device.mk
@@ -29,7 +29,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
