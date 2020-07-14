0871e0e18af66ee9e3b0a91f0f8816e0ed1b93c9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-07-14 15:40:39 +0200

    X00I: Inherit from common tree

---
+++ README.md
@@ -0,0 +1,19 @@
+# Device Tree for Asus Zenfone 4 Max (ZC554KL)
+
+## Spec Sheet
+
+| Feature                 | Specification                     |
+| :---------------------- | :-------------------------------- |
+| CPU                     | Octa-core 1.4 GHz Cortex-A53      |
+| Chipset                 | Qualcomm MSM8937 Snapdragon 430   |
+| GPU                     | Adreno 505                        |
+| Memory                  | 2/3 GB                            |
+| Shipped Android Version | 7.1.2                             |
+| Storage                 | 16/32 GB                          |
+| MicroSD                 | Up to 512 GB                      |
+| Battery                 | 4120 mAh (non-removable)          |
+| Dimensions              | 161.4 x 76.9 x 7.9 mm             |
+| Display                 | 1280x720 pixels, 5.0              |
+| Rear Camera             | 13 MP + 5MP, LED flash            |
+| Front Camera            | 8 MP                              |
+| Release Date            | October 2017                      |
---
+++ lineage_X00I.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit 64bit support
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from X00I device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := X00I
+PRODUCT_MANUFACTURER := asus
+PRODUCT_NAME := lineage_X00I
+PRODUCT_MODEL := Zenfone 4 Max
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+TARGET_VENDOR := asus
+TARGET_VENDOR_PRODUCT_NAME := X00ID
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="msm8937_64-user 9 PKQ1.181203.001 16.0200.1907.28-20190708 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := asus/WW_Phone/ASUS_X00ID:9/PKQ1.181203.001/16.0200.1907.28-20190708:user/release-keys
