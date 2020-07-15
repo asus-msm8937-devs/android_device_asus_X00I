d41bd37297892b49f3613ec84458e5fa2cd3b12b
deadman96385 <seanhoyt963@gmail.com>
2020-07-15 17:33:02 +0200

    X00I: Import device-specific overlays
    * Power profile from X00I Pie beta
    * Enable aux cameras on Snap
    * Enable hardware navbar support
    * Specify lights capabilities
    
    Change-Id: I54975899ed0499ac213f0a1588aa33eb0b1265dd

--- device.mk
+++ device.mk
@@ -24,6 +24,10 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
