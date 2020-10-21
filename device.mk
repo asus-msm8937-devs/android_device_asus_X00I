3ab7fee3d45a3ec53e6e6f029946e55242a88fee
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-10-21 08:51:46 +0200

    X00I: Move libunwind to vendor
    Change-Id: I1437f639b455cb1c8bf5564e16de096110d00c0c

--- device.mk
+++ device.mk
@@ -41,7 +41,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    libunwind
+    libunwind.vendor
 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.X00I
