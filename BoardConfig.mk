abd1fc4159403a58bc8d1bad6eb472639e87a03b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-05 21:49:30 +0200

    X00I: Assert on Pie firmware
    Change-Id: I4b82faaa398a61685782876ce1bcadd96e864618

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@ include device/asus/msm8937-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/asus/X00I
 
+# Assert
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+
 # Build
 BUILD_BROKEN_DUP_RULES := true
 
