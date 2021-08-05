c0f24297647a0c20fb44e42d3cbf68959717d11c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-05 21:46:22 +0200

    X00I: Add overlays
    * Power profile and light capabilities
    
    Change-Id: I3372c53211d5b5c9dbed4f109b87c3c9c3ce1f48

--- device.mk
+++ device.mk
@@ -18,6 +18,11 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
 # Audio configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
