58fc2d0024d3579b2a58bc36e15e43511d1e378c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-05 22:22:23 +0200

    X00I: Remove libandroid.so dependency on camera libs
    Change-Id: Ib699a7f4aa4ee9f281256a04c7658327a8c2dee3

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/libarcsoft_hdr.so \
+        |vendor/lib/libarcsoft_nighthawk.so \
+        |vendor/lib/libarcsoft_night_shot.so \
+        |vendor/lib/libarcsoft_panorama_burstcapture.so \
+        |vendor/lib/libarcsoft_videostab.so \
+        |vendor/lib/libmpbase.so)
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
