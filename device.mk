6dffe596482c66b2097584ea54ed0b48e675c17b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-13 00:06:14 +0200

    X00I: Add gx_fpd init service
    * From stock's init.target.rc, while at it renamed to vendor.gx_fpd
    
    Change-Id: Iceec51a796d269c4da685878c733716e26708378

--- device.mk
+++ device.mk
@@ -49,6 +49,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    vendor.gx_fpd.rc
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
